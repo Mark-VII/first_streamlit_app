a3fb3506d51f6d274e8f1f802df3ec9b7af76488
Mark-VII <117444641+Mark-VII@users.noreply.github.com>
2022-11-04 16:31:18 +0530

    Update streamlit_app.py
    One more header was added

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boild Free-Range Egg")
 streamlit.text("🥑🍞 Avakado Toast")
- 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
