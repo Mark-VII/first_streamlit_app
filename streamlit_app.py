a41ad42e446cc8772c7ffba306e2e95f7b95fcff
Mark-VII <117444641+Mark-VII@users.noreply.github.com>
2022-11-04 16:22:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.title("Kale, Spinach & Rocket Smoothie")
-streamlit.title("Hard-Boild Free-Range Egg")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boild Free-Range Egg")
