3d9af101032f4d4e725b32de82131f4a92cdc9ac
Mark-VII <117444641+Mark-VII@users.noreply.github.com>
2022-11-10 10:27:30 +0530

    Update streamlit_app.py
    Added code to enable custom inputs from users.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')#for users to enter a specific fruit
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # Lets make the jason data look a little nice.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
